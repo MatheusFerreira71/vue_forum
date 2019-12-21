e200359ea0c68cdb57d6ba84a81d8e76fc576326
MattSmith71 <matheusfernandofreire@hotmail.com>
2019-12-21 01:07:05 -0300

    Components de threads unicos e lista de threads criados e funcionais.

---
+++ src/components/ThreadListItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="thread">
+    <div>
+      <p>
+        <a href="#">{{thread.title}}</a>
+      </p>
+      <p class="text-faded text-xsmall">
+        By
+        <a href="#">{{user.name}}</a>
+        , at {{date}}
+      </p>
+    </div>
+
+    <div class="activity">
+      <p class="replies-count" v-if="repliesCount !== 1">{{repliesCount}} replies</p>
+      <p class="replies-count" v-else>{{repliesCount}} reply</p>
+
+      <!-- <img src alt="avatar" class="avatar-medium" /> -->
+
+      <!-- <div>
+        <p class="text-xsmall">
+          <a href="#"></a>
+        </p>
+        <p class="text-xsmall text-faded"></p>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+export default {
+  props: {
+    thread: {
+      required: true,
+      type: Object
+    }
+  },
+  computed: {
+    repliesCount () {
+      return Object.keys(this.thread.posts).length - 1
+    },
+    user () {
+      return sourceData.users[this.thread.userId]
+    },
+    date () {
+      return Date(this.thread.publishedAt)
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import ThreadShow from '@/components/ThreadShow'
 
 Vue.use(VueRouter)
 
@@ -11,17 +12,16 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/thread/:id',
+    name: 'ThreadShow',
+    component: ThreadShow,
+    props: true
   }
 ]
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 export default router
