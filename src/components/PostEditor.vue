0916f11571d329c1d4237a79e596ba7db4075958
MattSmith71 <matheusfernandofreire@hotmail.com>
2019-12-28 14:45:11 -0300

    Editor de post adicionado.

---
+++ src/components/PostEditor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form @submit.prevent="save">
+      <div class="form-group">
+        <textarea name id cols="30" rows="10" class="form-input" v-model="text"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    save () {
+      const postId = `greatPost${Math.random()}`
+      const post = {
+        text: this.text,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.threadId,
+        userId: '38St7Q8Zi2N1SPa5ahzssq9kbyp1',
+        '.key': postId
+      }
+      this.text = ''
+      this.$emit('save', { post })
+    }
+  },
+  data () {
+    return {
+      text: ''
+    }
+  },
+  props: {
+    threadId: {
+      required: true,
+      type: String
+    }
+  }
+}
+</script>
--- src/views/PageThreadShow.vue
+++ src/views/PageThreadShow.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="col-large push-top">
     <h1>{{thread.title}}</h1>
-    <PostList
-    :posts="posts"/>
+    <PostList :posts="posts"/>
+    <PostEditor
+    @save="addPost"
+    :threadId="id"
+    />
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
 import PostList from '@/components/PostList'
+import PostEditor from '@/components/PostEditor'
 export default {
   props: {
     id: {
@@ -17,7 +21,8 @@ export default {
     }
   },
   components: {
-    PostList
+    PostList,
+    PostEditor
   },
   data () {
     return {
@@ -27,6 +32,15 @@ export default {
   computed: {
     posts () {
       return Object.values(sourceData.posts).filter(post => Object.values(this.thread.posts).includes(post['.key']))
+    }
+  },
+  methods: {
+    addPost (eventData) {
+      const post = eventData.post
+      const postId = post['.key']
+      this.$set(sourceData.posts, postId, post)
+      this.$set(this.thread.posts, postId, postId)
+      this.$set(sourceData.users[post.userId].posts, postId, postId)
     }
   }
 }
